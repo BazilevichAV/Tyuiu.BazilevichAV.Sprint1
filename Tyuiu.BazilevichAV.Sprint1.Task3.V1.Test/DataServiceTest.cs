b9d20ab4a132300c651d318e3615d5cd97dab70e
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-09-28 11:45:16 +0500

    Task3

---
+++ Tyuiu.BazilevichAV.Sprint1.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BazilevichAV.Sprint1.Task3.V0.Lib;
+namespace Tyuiu.BazilevichAV.Sprint1.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double r = 2;
+            double h = 4;
+            double wait = 50.26548245743669;
+            var res = Math.Round(ds.CylinderVolume(r, h), 3);
+            Assert.AreEqual(Math.Round(wait, 3), res);
+        }
+    }
+}
---
+++ Tyuiu.BazilevichAV.Sprint1.Task3.V1/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.BazilevichAV.Sprint1.Task3.V0.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнила: Базилевич А.В. | ИБКСб-25-1";
+Console.WriteLine("********************************************************?**");
+Console.WriteLine("* Спринт #1                                               *");
+Console.WriteLine("* Тема: Операторы составного присваивания                 *");
+Console.WriteLine("* Задание #3                                              *");
+Console.WriteLine("* Вариант #1                                              *");
+Console.WriteLine("* Выполнила: Базилевич Алёна Владимировна | ИБКСб-25-1    *");
+Console.WriteLine("*******************************************************?***");
+Console.WriteLine("* УСЛОВИЕ:                                                *");
+Console.WriteLine("* Объявите необходимые переменные и напишите программу    *");
+Console.WriteLine("* вычисления объема цилиндра, предполагающий ввод исходных*");
+Console.WriteLine("* данных. Ответ округлите до 3 знаков после запятой.      *");
+Console.WriteLine("***********************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                        *");
+Console.WriteLine("***********************************************************");
+Console.WriteLine("* V = Math.PI * r * 2 * h                                 *");
+Console.WriteLine("***********************************************************");
+
+double r, h;
+
+Console.WriteLine("Введите значение r: ");
+r = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение h: ");
+h = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("***********************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                              *");
+Console.WriteLine("*******************************************************?***");
+
+Console.WriteLine("Объем цилиндра = " + Math.Round(ds.CylinderVolume(r, h), 3));
+Console.ReadLine();
