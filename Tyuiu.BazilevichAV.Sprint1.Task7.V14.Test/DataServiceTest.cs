034ec7d694a1afb56e5ebe5bbedf3820972ddc41
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-09-29 11:50:46 +0500

    Task7

---
+++ Tyuiu.BazilevichAV.Sprint1.Task7.V14.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BazilevichAV.Sprint1.Task7.V14.Lib;
+namespace Tyuiu.BazilevichAV.Sprint1.Task7.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = 2new DataService();
+            double x = 2;
+            double y = 4;
+            double wait = 1.449;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BazilevichAV.Sprint1.Task7.V14/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.BazilevichAV.Sprint1.Task7.V14.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнила: Базилевич А.В. | ИБКСб-25-1";
+Console.WriteLine("***********************************************************");
+Console.WriteLine("* Спринт #1                                               *");
+Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту *");
+Console.WriteLine("* Задание #7                                              *");
+Console.WriteLine("* Вариант #14                                             *");
+Console.WriteLine("* Выполнила: Базилевич Алёна Владимировна | ИБКСб-25-1    *");
+Console.WriteLine("***********************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет математическое    *");
+Console.WriteLine("* выражение по исходным значениям данных, вводимых        *");
+Console.WriteLine("* пользователем.Ответ округлите до 3 знаков после запятой.*");
+Console.WriteLine("***********************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                        *");
+Console.WriteLine("***********************************************************");
+Console.WriteLine("* z = 2^-x + 5x^2/3x^3 - cos(x)^2 + sin(2xy)              *");
+Console.WriteLine("***********************************************************");
+
+double x, y;
+
+Console.WriteLine("Введите значение x: ");
+x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение y: ");
+y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("***********************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                              *");
+Console.WriteLine("***********************************************************");
+
+Console.WriteLine(" z =  " + ds.Calculate(x, y));
+Console.ReadLine();
